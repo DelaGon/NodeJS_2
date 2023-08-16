b953642d88ab196dc95ffe9903f4e0d7ecf736a6
DelaGon <87527854+DelaGon@users.noreply.github.com>
2023-08-16 15:48:59 +0700

    Update app.js

--- nv-webblog-main/server/src/app.js
+++ nv-webblog-main/server/src/app.js
@@ -1,48 +1,32 @@
 let express = require('express')
 let bodyParser = require('body-parser')
 const {sequelize} = require('./models')
+
 const config = require('./config/config')
-
-
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-require('.route')(app)
+require('./routes')(app)
 
-app.get('/status', function (req, res) {
-res.send('Hello nodejs server')
+app.get('/status', function (req, res ){
+ res.send('Hello nodejs server')
 })
 
 app.get('/hello/:person', function (req,res) {
-console.log('hello - ' + req.params.person)
-res.send('sey hello with' + req.params.person)
-})
-//get user by id
-//app.get('/user/:userId', function (req, res) {
-//    res.send('ดูข้อมูลผู้ใช้งาน')
-//})
-
-//get all user
-//app.get('/user', function (req, res) {
- //   res.send('เรียกข้อมูลผู้ใช้งาน')
-//})
-
-app.post('/hello',function (req, res) {
-    res.send('OK you post-' + req.body.name)
-}
-)
-//let port = 8081
-
-//app.listen(port, function () {
-   // console.log('server running on ' + port)
-//})
-let port = process.env.PORT || config.port
-const {sequelize} = require('./models')
-sequelize.sync({force: false}).then(() => {
-app.listen(port, function () {
-console.log('Server running on ' + port)
-})
+ console.log('hello - ' + req.params.person)
+ res.send('sey hello with ' + req.params.person)
 })
 
+app.post('/hello', function (req, res) {
+    res.send('OK you post - ' + req.body.name)
+   })
+
+   let port = process.env.PORT || config.port
+
+   sequelize.sync({force: false}).then(() => {
+    app.listen(port, function () {
+    console.log('Server running on ' + port)
+    })
+   })
