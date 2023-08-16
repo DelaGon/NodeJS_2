de94120b9b6fa570c8a8c309e09876439371e92c
DelaGon <87527854+DelaGon@users.noreply.github.com>
2023-08-16 15:33:52 +0700

    ....

--- nv-webblog-main/server/src/app.js
+++ nv-webblog-main/server/src/app.js
@@ -1,5 +1,9 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+const {sequelize} = require('./models')
+const config = require('./config/config')
+
+
 const app = express()
 
 app.use(bodyParser.json())
@@ -29,9 +33,14 @@ app.post('/hello',function (req, res) {
     res.send('OK you post-' + req.body.name)
 }
 )
-let port = 8081
+//let port = 8081
 
-app.listen(port, function () {
-    console.log('server running on ' + port)
+//app.listen(port, function () {
+   // console.log('server running on ' + port)
+//})
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(port, function () {
+    console.log('Server running on ' + port)
+    })
 })
-
